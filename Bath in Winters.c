24c204c3036e285cb2eec912591c17c216d58354
KARTIK JAIN <115203884+Kartikk-26@users.noreply.github.com>
2023-08-26 11:39:52 +0530

    Create Bath in Winters.c

---
+++ Bath in Winters.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int x,y,z=0;
+	    scanf("%d%d",&x,&y);
+	    printf("%d\n",x/(2*y));
+	   
+	}
+	return 0;
+}
+
