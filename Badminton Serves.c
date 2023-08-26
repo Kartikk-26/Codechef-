95239e149643868f0e0fe0b0997c045034a4ecee
KARTIK JAIN <115203884+Kartikk-26@users.noreply.github.com>
2023-08-26 11:25:48 +0530

    Create Badminton Serves.c

---
+++ Badminton Serves.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int p,count=0;
+	    scanf("%d",&p);
+	    printf("%d\n",p/2+1);
+	}
+	return 0;
+}
+
