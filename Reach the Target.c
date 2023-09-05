a053b318972e8bc5cee7cd838dc7576fe2f65907
KARTIK JAIN <115203884+Kartikk-26@users.noreply.github.com>
2023-09-05 16:37:19 +0530

    Create Reach the Target.c

---
+++ Reach the Target.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int T,X,Y;
+	scanf("%d",&T);
+	for(int i=0;i<T;i++)
+	{
+	    scanf("%d %d",&X,&Y);
+	    
+	        printf("%d\n",(X-Y));
+	    
+	    
+	}
+	return 0;
+}
+
