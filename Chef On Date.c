bb2932f0fdb283e85239d94ba7367cec53da517b
KARTIK JAIN <115203884+Kartikk-26@users.noreply.github.com>
2023-09-10 07:38:43 +0530

    Create Chef On Date.c

---
+++ Chef On Date.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int x,y;
+        scanf("%d%d",&x,&y);
+        if(x>=y)
+        printf("\nYES");
+        else
+        printf("\nNO"); }
+	// your code goes here
+	return 0;
+}
+
