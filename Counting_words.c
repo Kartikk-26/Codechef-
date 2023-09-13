b1e1985824c2e2d2fc350123f07d58af783dd812
KARTIK JAIN <115203884+Kartikk-26@users.noreply.github.com>
2023-09-13 18:51:08 +0530

    Create Counting_words.c

---
+++ Counting_words.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int a,b;
+        scanf("%d%d",&a,&b);
+printf("\n%d",a*b); }
+	// your code goes here
+	return 0;
+}
+
