0fc86f26499bc64c91640ba219af7ec0c5ba4852
KARTIK JAIN <115203884+Kartikk-26@users.noreply.github.com>
2023-09-15 09:27:27 +0530

    Difficulty Level 300

---
+++ Tom & Jerry.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int x,y;
+	    scanf("%d%d",&x,&y);
+	    if(x<y)
+	    printf("YES\n");
+	    else 
+	    printf("NO\n");
+	}
+	return 0;
+}
+
