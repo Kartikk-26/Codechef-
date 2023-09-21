b8ccac2b7f4ed7e2da510fa2c20f64d836f20b79
KARTIK JAIN <115203884+Kartikk-26@users.noreply.github.com>
2023-09-21 11:31:41 +0530

    Create Cheif & Brain.c

---
+++ Cheif & Brain.c
@@ -0,0 +1,11 @@
+/* package codechef; // don't place package name! */
+
+#include<stdio.h>
+main(){
+    int a,b;
+    scanf(" %d%d",&a,&b);
+    if(b>a)
+    printf("Yes");
+    else
+    printf("No");
+} 
