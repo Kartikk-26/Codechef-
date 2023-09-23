9e5ad2ff2a8a0cdd362ee800d38e0e5ca5fa8475
KARTIK JAIN <115203884+Kartikk-26@users.noreply.github.com>
2023-09-23 18:27:18 +0530

    Create ATM Machine.c

---
+++ ATM Machine.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int n,k;
+	    scanf("%d%d",&n,&k);
+	    int a[n];
+	    for(int i=0;i<n;i++){
+	        scanf("%d",&a[i]);
+	    }
+	    for(int i=0;i<n;i++)
+	    {
+	        if(a[i]<=k){
+	            k=k-a[i];
+	            printf("1");
+	        }
+	        else{
+	            printf("0");
+	        }
+	     
+	    }
+	    printf("\n");
+	    
+	}
+	return 0;
+}
+
