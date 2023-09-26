693daf13541e196132015c5a13eed3944f660609
KARTIK JAIN <115203884+Kartikk-26@users.noreply.github.com>
2023-09-26 17:40:18 +0530

    Difficulty Rating : 299

---
+++ Right There.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    scanf("%d",&t);
+    
+    while(t--)
+    {
+    int x,y;
+     scanf("%d%d",&x,&y);
+
+        if(x<=y)
+        {
+            printf("YES\n");
+        }
+        
+        else
+{
+    
+    printf("NO\n");
+}
+    }
+	return 0;
+}
+
