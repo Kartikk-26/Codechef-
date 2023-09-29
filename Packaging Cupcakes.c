8f575bfba37e8e475cb98d9bb91a8435715f78b0
KARTIK JAIN <115203884+Kartikk-26@users.noreply.github.com>
2023-09-29 09:43:28 +0530

    Difficulty Rating : 1023

---
+++ Packaging Cupcakes.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int n;
+	    scanf("%d",&n);
+	    if(n%2==0)
+	    printf("%d\n",(n/2)+1);
+	    else
+	    printf("%d\n",(n+1)/2);
+	}
+	return 0;
+}
+
