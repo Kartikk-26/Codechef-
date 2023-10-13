15aff144858b87afe1b652e3ebeaa2212cc29c19
KARTIK JAIN <115203884+Kartikk-26@users.noreply.github.com>
2023-10-13 09:49:01 +0530

    Difficulty Rating : 1018

---
+++ Candies.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void) {
+	int t;
+	scanf("%d\n", &t);
+	while(t--){
+	    int n;
+	    scanf("%d\n", &n);
+	    int m=2*n;
+	    int a[m],count;
+	    for(int i=0;i<m;i++){
+	        scanf("%d", &a[i]);
+	    }
+	    for(int i=0;i<m;i++){
+	        count=0;
+	        for(int j=0;j<m;j++){
+	            if(a[j]==a[i]){
+	                count++;
+	            }
+	        }
+	        if(count>2){
+	            break;
+	        }
+	    }
+	    if(count>2){
+	        printf("No\n");
+	    }
+	    else{
+	        printf("Yes\n");
+	    }
+	}
+	return 0;
+}
+
