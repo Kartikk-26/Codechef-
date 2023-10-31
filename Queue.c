0900e4e518a028dbdd53587a7db58634691a3c42
KARTIK JAIN <115203884+Kartikk-26@users.noreply.github.com>
2023-10-31 20:26:44 +0530

    Add files via upload

---
+++ Queue.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<process.h>
+int queue[50];
+int rear=-1;
+int front=-1;
+void enqueue();
+void dequeue();
+void display();
+
+int main(){
+
+}
+
+void enqueue(){
+    if(rear==49)
+        
+     
+}
+void dequeue(){
+    
+}
