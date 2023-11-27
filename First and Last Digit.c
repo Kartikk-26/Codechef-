fbf2cb509bd1c55f8b4ed1c4f1555baf6eeee100
KARTIK JAIN <115203884+Kartikk-26@users.noreply.github.com>
2023-11-27 23:07:59 +0530

    Create First and Last Digit.c

---
+++ First and Last Digit.c
@@ -0,0 +1,18 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int ld,n,t,r;
+    
+    scanf("%d",&t);
+    while(t--){
+        scanf("%d",&n);
+        ld = n%10;
+        while(n>0) {r=n%10; n/=10;}
+        printf("%d\n",r+ld);
+    }
+    return 0;
+}
+
